0fb664449725a38cae5cfb2391f4ebdf948a4642
mamulini <mamulini@yahoo.com>
2020-03-19 20:47:28 +0200

    Updates

---
+++ precache-manifest.0a3790cc6f1c5f6640c204161da48e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25950c7c6a81cc938d2d64d1a242275a",
+    "url": "/movieslist/index.html"
+  },
+  {
+    "revision": "f755f5a15c4f0cf2440a",
+    "url": "/movieslist/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "640552b82ad2100a567c",
+    "url": "/movieslist/static/css/main.9b53198d.chunk.css"
+  },
+  {
+    "revision": "f755f5a15c4f0cf2440a",
+    "url": "/movieslist/static/js/2.71932234.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movieslist/static/js/2.71932234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640552b82ad2100a567c",
+    "url": "/movieslist/static/js/main.3279779e.chunk.js"
+  },
+  {
+    "revision": "369fd950ea49fa0c78e2",
+    "url": "/movieslist/static/js/runtime-main.879fffa0.js"
+  }
+]);
